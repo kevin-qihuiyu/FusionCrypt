c38b660b11cb5acc3855ed8b810a927476dfe6bb
kevin-qihuiyu <qihui.yu0811@gmail.com>
2023-04-17 15:52:09 +0200

    Create CoinImageView/ ViewModel/ CoinImageService to handle coin image download

---
+++ FusionCrypt/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageViewModel.swift
+//  FusionCrypt
+//
+//  Created by qihuiyu on 17/04/2023.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        self.addSubscribers()
+        self.isLoading = true
+    }
+    
+    func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: {[weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+    }
+}
